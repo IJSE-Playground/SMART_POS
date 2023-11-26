8d976ef94904bf9dd67b2f9bb83f7871b15d8f81
isuri <isuriwgunaratne@gmail.com>
2023-11-26 13:41:53 +0530

    Complete UI Design

--- src/main/java/lk/ijse/dep11/pos/controller/PlaceOrderController.java
+++
@@ -1,4 +0,0 @@
-package lk.ijse.dep11.pos.controller;
-
-public class PlaceOrderController {
-}
--- src/main/java/lk/ijse/dep11/pos/controller/PlaceOrderFormController.java
+++ src/main/java/lk/ijse/dep11/pos/controller/PlaceOrderFormController.java
@@ -1,12 +1,14 @@
 package lk.ijse.dep11.pos.controller;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
@@ -15,15 +17,21 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 
-public class ManageItemFormController {
+public class PlaceOrderFormController {
     public AnchorPane root;
-    public JFXTextField txtCode;
+    public JFXTextField txtCustomerName;
     public JFXTextField txtDescription;
     public JFXTextField txtQtyOnHand;
     public JFXButton btnSave;
-    public JFXButton btnDelete;
-    public TableView tblItems;
+    public TableView tblOrderDetails;
     public JFXTextField txtUnitPrice;
+    public JFXComboBox cmbCustomerId;
+    public JFXComboBox cmbItemCode;
+    public JFXTextField txtQty;
+    public Label lblId;
+    public Label lblDate;
+    public Label lblTotal;
+    public JFXButton btnPlaceOrder;
 
     public void navigateToHome(MouseEvent mouseEvent) throws IOException {
         URL resource = this.getClass().getResource("/view/MainForm.fxml");
@@ -35,12 +43,12 @@ public class ManageItemFormController {
         Platform.runLater(primaryStage::sizeToScene);
     }
 
-    public void btnAddNew_OnAction(ActionEvent actionEvent) {
+    public void btnAdd_OnAction(ActionEvent actionEvent) {
     }
 
-    public void btnSave_OnAction(ActionEvent actionEvent) {
+    public void txtQty_OnAction(ActionEvent actionEvent) {
     }
 
-    public void btnDelete_OnAction(ActionEvent actionEvent) {
+    public void btnPlaceOrder_OnAction(ActionEvent actionEvent) {
     }
 }

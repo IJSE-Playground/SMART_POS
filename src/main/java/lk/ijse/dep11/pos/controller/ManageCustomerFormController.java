82ff6e3b207e048c606ae89d30a6f4a812e7f959
isuri <isuriwgunaratne@gmail.com>
2023-11-27 09:22:09 +0530

    Add Customer Table Model and Map it with the Columns

--- src/main/java/lk/ijse/dep11/pos/controller/ManageCustomerFormController.java
+++ src/main/java/lk/ijse/dep11/pos/controller/ManageCustomerFormController.java
@@ -1,5 +1,4 @@
 package lk.ijse.dep11.pos.controller;
-
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.application.Platform;
@@ -8,13 +7,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import lk.ijse.dep11.pos.tm.Customer;
 
 import java.io.IOException;
 import java.net.URL;
-
 public class ManageCustomerFormController {
     public AnchorPane root;
     public JFXTextField txtCustomerId;
@@ -22,7 +23,7 @@ public class ManageCustomerFormController {
     public JFXTextField txtCustomerAddress;
     public JFXButton btnSave;
     public JFXButton btnDelete;
-    public TableView tblCustomers;
+    public TableView<Customer> tblCustomers;
 
     public void navigateToHome(MouseEvent mouseEvent) throws IOException {
         URL resource = this.getClass().getResource("/view/MainForm.fxml");
@@ -34,12 +35,23 @@ public class ManageCustomerFormController {
         Platform.runLater(primaryStage::sizeToScene);
     }
 
+    public void initialize(){
+        tblCustomers.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
+        tblCustomers.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("name"));
+        tblCustomers.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("address"));
+        txtCustomerId.setEditable(false);
+        btnDelete.setDisable(true);
+    }
+
     public void btnAddNew_OnAction(ActionEvent actionEvent) {
+        for (TextField textField : new TextField[]{txtCustomerId, txtCustomerName, txtCustomerAddress})
+            textField.clear();
+        tblCustomers.getSelectionModel().clearSelection();
+        txtCustomerName.requestFocus();
     }
 
     public void btnSave_OnAction(ActionEvent actionEvent) {
     }
-
     public void btnDelete_OnAction(ActionEvent actionEvent) {
     }
 }

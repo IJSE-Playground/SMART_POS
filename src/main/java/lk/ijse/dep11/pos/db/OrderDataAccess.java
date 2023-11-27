7becdae5a4b75a34f50ffe5615544253f27a6e3c
isuri <isuriwgunaratne@gmail.com>
2023-11-27 14:23:37 +0530

    Add getLastOrderId()

--- src/main/java/lk/ijse/dep11/pos/db/OrderDataAccess.java
+++ src/main/java/lk/ijse/dep11/pos/db/OrderDataAccess.java
@@ -3,21 +3,31 @@ package lk.ijse.dep11.pos.db;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 
 public class OrderDataAccess {
-
     private static final PreparedStatement STM_EXISTS_BY_CUSTOMER_ID;
     private static final PreparedStatement STM_EXISTS_BY_ITEM_CODE;
+    private static final PreparedStatement STM_GET_LAST_ID;
 
     static {
         try {
             Connection connection = SingleConnectionDataSource.getInstance().getConnection();
             STM_EXISTS_BY_CUSTOMER_ID = connection.prepareStatement("SELECT * FROM \"order\" WHERE customer_id = ?");
             STM_EXISTS_BY_ITEM_CODE = connection.prepareStatement("SELECT * FROM order_item WHERE item_code = ?");
+            STM_GET_LAST_ID = connection.prepareStatement("SELECT id FROM \"order\" ORDER BY id DESC FETCH FIRST ROWS ONLY");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
+
+
+    public static String getLastOrderId() throws SQLException{
+        ResultSet rst = STM_GET_LAST_ID.executeQuery();
+        return (rst.next())? rst.getString(1): null;
+    }
+
+
 
     public static boolean existsOrderByCustomerId(String customerId) throws SQLException {
         STM_EXISTS_BY_CUSTOMER_ID.setString(1, customerId);
